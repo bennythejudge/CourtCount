5fd54d91c002cd5cbb3416fbaa7db9948bae42f7
Benedetto Lo Giudice <logube@gmail.com>
2017-11-24 13:20:50 +0100

    Connect buttons to methods

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitCommand(View view) {
+        displayForTeamA(3);
+        ((Button) view).getText();
+    }
+
+    public void submitCommand2(View view) {
+        displayForTeamA(2);
+    }
+
+    public void submitCommand1(View view) {
+        displayForTeamA(1);
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreview = (TextView) findViewById(R.id.team_a_score);
+        scoreview.setText(String.valueOf(score));
     }
 }
